3272311e239d438248630fee0391cf1cfceac00a
Shubham Mandal <shubhammandal13579@gmail.com>
2025-11-13 09:19:42 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all js code as newer version.
+
+//alert(3+4)  // we r using nodejs,not browser.
+
+// code should readbale and futureproof,hence maintain proper spapces and avoide faltu next lines .
+
+//original documentaion for js (standard follow in js) --- tc39.es   ( use mdn also but prefer lessly agr ho ske th. )
+
+let name = "shubham"
+let age= "21"
+let isLoggedin = false
+
+// datatypes..
+// number
+// bigint
+//string
+//boolean => false/ true
+//null => standalone value
+//undefined
+//symbol => uniqueness
+
+// object
+
+console.log(typeof null);      // object
+console.log(typeof undefined);   // undefined
+
+
