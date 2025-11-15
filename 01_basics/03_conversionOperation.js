0bd98699740d98771ce537b1fa64ce12a423cf73
Shubham Mandal <shubhammandal13579@gmail.com>
2025-11-15 06:06:58 +0000

    conversio of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "123abc"   // place the values to check its type and conversion take place or not .
+
+//console.log( typeof(score));
+ 
+let valueInscore = Number (score);
+//console.log(typeof(valueInscore));
+//console.log(valueInscore);
+
+// "33" => 33
+// "33abc" => NaN ...etc
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1=> true;  0 => false
+// "" => false
+// "shubham" => true
+
+let someNumber = 33
+console.log(someNumber);
+console.log(typeof(someNumber));
+
+let  stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+ 
+
+
