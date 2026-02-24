6c6e6a8d133773ce346a5fb5cca7357311b87d0a
Shubham Mandal <shubhammandal13579@gmail.com>
2026-02-24 03:25:33 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,4 +30,41 @@ function loginUsername(username = "sam"){
     return (`${username} ,just loged in` )
 }
 // console.log(loginUsername());
-console.log(loginUsername("raaaj"));   //raaj will overwrite in the place of 'sam'
+//console.log(loginUsername("raaaj"));   //raaj will overwrite in the place of 'sam'
+
+// jab argument bhut jyda ho .....
+
+function calculateCartprice(...num){
+    return num;
+}  //... using as rest operator
+//console.log(calculateCartprice(200,300,100));
+
+function calculateCartprice(val1,val2,...num){
+    return num;
+} 
+//console.log(calculateCartprice(100,200,300,400));   // here 100,200 goes to val1 ,val2 respectively.
+
+
+// passing objects and array in function as argument.
+const user1 = {
+    username :"shubham",
+    price : 100
+}
+
+function handleObject(anyobject){
+    return (`the price for the cart of ${anyobject.username} is ${anyobject.price}. `)
+}
+// console.log(handleObject(user1));
+// console.log(handleObject({
+//     username: "raj",
+//     price :200
+// }));
+
+const myArray = [100,200,300,4000]
+
+function handleArray(anyarray){
+    return anyarray[1] // let assume we need 2nd value from the array.
+}
+
+//console.log(handleArray(myArray));
+console.log(handleArray([1,2,3,4,5,6]));
