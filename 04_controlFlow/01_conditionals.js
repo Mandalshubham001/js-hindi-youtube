edf21f0b10e2fe808e730b4678180ecfdd6a18cb
Shubham Mandal <shubhammandal13579@gmail.com>
2026-02-28 09:05:58 +0000

    conditional

---
+++ 04_controlFlow/01_conditionals.js
@@ -0,0 +1,51 @@
+// conditional statements.............
+// if
+
+ const isuserloggedIn = true
+  
+//  if(true){
+//     //code will runn
+//  }
+
+// if(isuserloggedIn){
+//     console.log("yes u are loggedin");
+    
+// }
+
+//  <, >, <=, >=, ==, !=, ===
+
+// if(2 === '2'){
+//     console.log('yes u are loggedin');
+    
+// }      // will not runn bcz '===' checks strictly......
+
+const temp = 500
+
+// if(temp < 100){
+//     console.log("less than 100");
+    
+// }else{
+//     console.log("greater than 100");
+    
+// }
+
+// if(temp < 100){
+//     console.log("less than 100");
+    
+// }else if(temp === 100){
+//     console.log(" equal to 100");
+    
+// }else if(temp == 40){
+//     console.log("equal to 40");
+// }else{
+//     console.log("greater than 100");
+    
+// }
+
+const userloggedin = true
+const debitCard = true
+
+if (userloggedin && debitCard){
+    console.log("allow  to purchase the course.");
+    
+}
