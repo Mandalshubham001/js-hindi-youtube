685c1e307536f3e0f58e524d888af04849c827f3
Shubham Mandal <shubhammandal13579@gmail.com>
2026-03-01 07:36:55 +0000

    do while and while

---
+++ 05_iteration/two.js
@@ -0,0 +1,14 @@
+// while and do while loop............................
+
+// let index = 0
+// while(index <=10){
+//     console.log(`value of index is ${index}`);
+//     index += 2  
+// }
+
+let score = 9
+do {
+    console.log(`value of score is ${score}`);
+    score ++
+    
+} while( score <= 8);
